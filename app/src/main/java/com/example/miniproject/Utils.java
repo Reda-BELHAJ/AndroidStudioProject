d37f382e08cd0833affe0c699f2d67cfc6460a25
Reda BELHAJ <58605073+Reda-BELHAJ@users.noreply.github.com>
2021-07-29 21:00:38 +0100

    Update Utils.java

--- app/src/main/java/com/example/miniproject/Utils.java
+++ app/src/main/java/com/example/miniproject/Utils.java
@@ -2,5 +2,5 @@ package com.example.miniproject;
 
 public class Utils {
     public static final String Email = "projet.mini99@gmail.com";
-    public static final String Password = "Android@mini99";
+    public static final String Password = "";
 }
